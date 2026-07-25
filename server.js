8de6c0c2e6d4b5c510d9da72bf56e2622935d37b
herwal12 <herwal12@gmail.com>
2026-07-25 00:47:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ client.on('interactionCreate', async (interaction) => {
 
     if (action === 'accept') {
       await targetUser.send({
-        content: `Félicitation ! Votre candidature visant la modération de notre serveur a été accepté !\nIl vous est donc demandé d'ouvrir un ticket sur le Discord principal, dans la catégorie Direction, afin de poursuivre les formalités. Merci de ne faire aucune mention (@) dans votre ticket.`
+ content: `Félicitation ! Votre candidature visant la modération de notre serveur a été accepté !\nIl vous est donc demandé d'ouvrir un ticket sur le Discord principal, dans la catégorie Direction, afin de poursuivre les formalités. Merci de ne faire aucune mention (@) dans votre ticket.`
       }).catch(() => console.log("Impossible d'envoyer un MP au membre (MP fermés)."));
 
       originalEmbed.setColor(0x2ED573); // Vert
